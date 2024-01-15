996081a27433566d204a3bb8adf899bbf9043647
bharb <badrh@motory.com>
2024-01-15 20:28:09 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.devtools.ksp") version "1.8.0-1.0.8"
+}
+
+apply(rootDir.path + "/dependencies.gradle")
+apply(rootDir.path + "/sub_project.gradle")
+
+android {
+    namespace = "com.bader.harb.chainreactiontask"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.bader.harb.chainreactiontask"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isDebuggable = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+}
+
+dependencies {
+    /*implementation(project(":features:splash:presentation"))
+    implementation(project(":features:main:presentation"))
+    implementation(project(":features:auth:presentation"))
+    implementation(project(":features:news:presentation"))
+    implementation(project(":features:more:presentation"))
+    implementation(project(":core:component"))
+    implementation(project(":core:storage"))*/
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
